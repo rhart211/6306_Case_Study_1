af2f707329e1fa84dc3ee7cc5d0dddf58c88b3d5
Richard Hart <6999074+rhart211@users.noreply.github.com>
2019-02-15 17:32:05 -0500

    answered questions 1-4

--- R/richard_sandbox.R
+++ R/richard_sandbox.R
@@ -1,8 +1,12 @@
 # Richard's Sandbox for saving R commands/history
 
+# Load Libraries
+library(plyr)
+library(ggplot2)
+
 # Create Data Frames from CSV files
-beers <- read.csv(file="data/Beers.csv", header = TRUE, sep = ",")
-breweries <- read.csv(file = "data/Breweries.csv", header = TRUE, sep = ",")
+beers <- read.csv(file="data/Beers.csv", stringsAsFactors=FALSE, header = TRUE, sep = ",")
+breweries <- read.csv(file = "data/Breweries.csv", stringsAsFactors=FALSE, header = TRUE, sep = ",")
 
 # Check the Class of each 
 class(beers)
@@ -21,3 +25,25 @@ names(breweries)[2] <- "Brewery_name"
 names(beers)[1] <- "Beer_name"
 
 
+# Question 1: number of breweries in each state
+state_beers <- table(breweries$State)
+state_beers
+
+# Question 2
+# Merge Dataset together to be used to answer questions
+beers_brews <- merge(beers, breweries, by="Brewery_id", all=TRUE)
+head(beers_brews, 6)
+
+# Question 3 Report the number of NAs in each column
+colSums(is.na(beers_brews))
+
+# Question 4 Compute the median alcohol content and international 
+# bitterness unit for each state. Plot a bar chart to compare.
+median_abv <- median(beers_brews$ABV, na.rm = TRUE)
+median_ibu <- median(beers_brews$IBU, na.rm = TRUE)
+
+library(cowplot)
+par(mfrow=c(1,2))
+plot1 <- ggplot(beers, aes(x= ABV)) + geom_bar(fill="steelblue")+xlab("Alcohol by Volume")+ylab("Number of Beers")+ggtitle("Distribution of ABV")
+plot2 <- ggplot(beers, aes(x= IBU)) + geom_bar(fill="brown2")+xlab("International Bitterness Units")+ylab("Number of Beers")+ggtitle("Distribution of IBU")
+plot_grid(plot1, plot2)
